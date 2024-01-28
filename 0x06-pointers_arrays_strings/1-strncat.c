2c0c3a8045c0e011c8929e9b19723434be92d52f
lilcrystal001 <stephen2000michael@gmail.com>
2024-01-27 19:00:34 -0800

    added the 1-strncat.c file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings,
+ * @dest: destination parameter.
+ * @src: source parameter.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int counter = 0,
+	int counter2 = 0;
+
+	while (*(dest + counter) != '\0')
+	{
+		count++;
+	}
+
+	while (counter2 < n)
+	{
+		*(dest + counter) = *(src + counter2);
+		if (*(src + counter2) == '\0')
+			break;
+		counter++;
+		counter2++;
+	}
+	return (dest);
+}
