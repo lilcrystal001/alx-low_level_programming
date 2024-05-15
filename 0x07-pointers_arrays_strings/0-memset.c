7a8e5128445a3b5c1544a208d34161c7d5ab87d3
lilcrystal001 <stephen2000michael@gmail.com>
2024-05-15 02:42:58 -0700

    added the fle 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memset- this fills memory with a constant amount of byte
+ * @s: memory section to be filled
+ * @b: char to copy
+ * @n: number of times b is to be copied
+ *
+ * Return: Pointer to the memory address of s
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int s)
+{
+	unsigned int i,
+		    
+	for (i = 0,i < n, i++)
+	{
+		s[i] = b;
+	}
+	
+	return (s);
+}
