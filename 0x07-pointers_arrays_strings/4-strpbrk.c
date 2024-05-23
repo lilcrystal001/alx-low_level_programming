840b15ad4e24cd60bce7b2bbb43de1d7da913565
lilcrystal001 <stephen2000michael@gmail.com>
2024-05-22 21:38:53 -0700

    modified 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,9 @@
 include "main.h"
 
 /**
- * strpbrk - function that searches a string for any of a set of bytes
+ * _strpbrk - function that searches a string for any of a set of bytes
  *
- * @s: first occurrence in the string 
+ * @s: first occurrence in the string
  *
  * @accept: matches one of the bytes, or @NULL if no such byte
  *
